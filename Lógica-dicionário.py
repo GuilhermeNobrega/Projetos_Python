cf1a6dcc6b1c7d41ac75a8ac20e92459ec86c86b
Guilherme Nóbrega Gomes Dantas <100214869+GuilhermeNobrega@users.noreply.github.com>
2023-08-28 10:27:19 -0300

    Create Lógica-dicionário.py

---
+++ Lógica-dicionário.py
@@ -0,0 +1,33 @@
+# Below are the examples of getting the index of specified Key
+
+# Initialize dictionary
+my_dict = {'course' : 'Python', 'fee' : 4500, 'duration' : '45 days'}
+
+# Example 1: Get the index position of the specified key
+# In Dictionary using list() and index()
+# Initialize the index
+index = None
+if "fee" in my_dict:
+    index = list(my_dict).index("course")
+print("Index of fee:", index) 
+
+
+#-----------------------------
+
+
+# Initialize dictionary
+my_dict = {'course': 'Python', 'fee': 4500, 'duration': '45 days'}
+
+# Specify the key you're looking for
+key_to_find = 'fee'
+
+# Use the get() method to retrieve the value and index
+if key_to_find in my_dict:
+  index = list(my_dict).index(key_to_find)
+else:
+  index = None
+
+if index is not None:
+    print(f"Index of {key_to_find}: {index}")
+else:
+    print(f"The key '{key_to_find}' is not present in the dictionary.")
